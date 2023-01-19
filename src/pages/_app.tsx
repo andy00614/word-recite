b375e784e69c2c77e129f9acc850b6264b038bb4
andy00614 <33732843@qq.com>
2023-01-19 23:58:51 +0800

    fix: backfill antd version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
+import 'antd/dist/antd.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+  </>
 }
